b2e17cada1f29b3a18498077f0b538c7b9d082c4
Abolfazleyaghoobi <134126450+Abolfazleyaghoobi@users.noreply.github.com>
2025-02-18 09:28:50 +0330

    finish header

--- src/components/MyImage/MyImage.js
+++ src/components/MyImage/MyImage.js
@@ -1,66 +1,38 @@
 import { Col, Container, Row } from "react-bootstrap";
-import ffc from "../../assets/image/pic-min.png"
+import ffc from "../../assets/image/pic-min.png";
 import gsap from "gsap";
 
 import "./MyImage.css";
-import Typewriter from "typewriter-effect";
 import { useEffect, useState } from "react";
 import BioMe from "./bio Header/BioHeader";
 import { useGSAP } from "@gsap/react";
 
 function MyImage() {
   useGSAP(() => {
-  
     const timeline = gsap.timeline();
     timeline.to(".boxshadow", {
-      x: 0,
+      left:0,
       opacity: 1,
       duration: 1.9,
+      visibility: "visible",
     });
-
   });
-
 
   return (
     <div>
       <Container>
-        <Row className="align-items-center">
-          <Col className=" ">
-            <h2>
-              سلام من{" "}
-              <span style={{ color: "#ff014f", fontWeight: "bolder" }}>
-                ابوالفضل هستم
-              </span>
-            </h2>
-            <h5 className="d-flex ">
-              <span style={{ color: "#ff014f" }} className="text-bold">
-                یک
-              </span>
-              <div style={{ color: "#000", fontWeight: "bolder" }}>
-                <Typewriter
-                  options={{
-                    strings: [
-                      "توسعه دهنده فرانت اند"
-                      ,
-                      ""
-                    ],
-                    autoStart: true,
-                 stringSplitter:Row,
-                    delay: 20,
-                    loop:true,
-                    deleteSpeed: 50,
-                  }}
-                />
-              </div>
-            </h5>
-            <BioMe />
-          </Col>
-          <Col className="">
-            <div className="boxshadow">
-              <div className="conImage m-auto">
-                <img className="img-fluid MyImage" src={ffc} alt="" />
+        <Row className="align-items-center row-cols-1 row-cols-md-2 gridee">
+          <Col className="position-relative">
+            <div className="contai">
+              <div className="boxshadow">
+                <div className="conImage m-auto">
+                  <img className="img-fluid MyImage" src={ffc} alt="" />
+                </div>
               </div>
             </div>
+          </Col>
+          <Col className=" ">
+            <BioMe />
           </Col>
         </Row>
       </Container>
--- src/components/MyImage/bio Header/BioHeader.js
+++ src/components/MyImage/bio Header/BioHeader.js
@@ -2,11 +2,41 @@ import { TiSocialLinkedin } from "react-icons/ti";
 import { FaReact } from "react-icons/fa";
 import reactIcons from "../../../assets/image/logo/icons8_react_2.svg";
 import htmlIcons from "../../../assets/image/logo/icons8_html_5_2.svg";
-import JSIcons from "../../../assets/image/logo/icons8_javascript.svg";
+import JSIcons from "../../../assets/image/logo/icons8_javascript_1.svg";
 import "./BioHeader.css";
+import Typewriter from "typewriter-effect";
+
+import { Link } from "react-router-dom";
 function BioMe() {
   return (
     <>
+      <h2>
+              سلام من{" "}
+              <span style={{ color: "#ff014f", fontWeight: "bolder" }}>
+                ابوالفضل هستم
+              </span>
+            </h2>
+            <h5 className="d-flex ">
+              <span style={{ color: "#ff014f" }} className="text-bold">
+                یک
+              </span>
+              <div style={{ color: "#000", fontWeight: "bolder" }}>
+                <Typewriter
+                  options={{
+                    strings: [
+                      "توسعه دهنده فرانت اند"
+                      ,
+                      ""
+                    ],
+                    autoStart: true,
+                 
+                    delay: 20,
+                    loop:true,
+                    deleteSpeed: 50,
+                  }}
+                />
+              </div>
+            </h5>
       <p>
         به‌عنوان برنامه‌نویس وب فعالیت می‌کنم. من علاقه به زبان جاوااسگریپت دارم
         و علاقه زیادی به طراحی و توسعه وب‌سایت‌های کاربرپسند دارم. همیشه سعی
@@ -15,8 +45,8 @@ function BioMe() {
         اگر به دنبال همکاری یا صحبت در مورد پروژه‌های وب هستید، خوشحال می‌شوم با
         شما در ارتباط باشم.
       </p>
-      <div className="row align-items-center">
-        <div className="sial-media col">
+      <div className="row row-cols-1 row-cols-xl-2 align-items-center">
+        <div className="sial-media col ">
           <b>پیدا کردن من:</b>
           <div className="linkedin mt-2">
             <a
@@ -27,7 +57,7 @@ function BioMe() {
             </a>
           </div>
         </div>
-        <div className="col">
+        <div className="col skill">
           <b>بهترین مهارت های من:</b>
           <div className="skills d-flex mt-2">
             {/* React Skil */}
@@ -35,21 +65,24 @@ function BioMe() {
               <a className="text-dark">
                 <img className="img-fluid" src={reactIcons} alt="" />
               </a>
-              <b>React</b>
+             <p> <b>React</b></p>
             </div>
             {/* HTML Skil */}
             <div className="html mt-2 mx-2">
               <a className="text-dark">
                 <img className="img-fluid" src={htmlIcons} alt="" />
               </a>
-              <b>HTML5</b>
+             <p> <b>HTML5</b></p>
             </div>
+            {/* js */}
             <div className="js mt-2 mx-2">
               <a className="text-dark">
                 <img className="img-fluid" src={JSIcons} alt="" />
               </a>
-              <b>JS</b>
+          <p><b>JS</b></p>
             </div>
+            {/* more */}
+           
           </div>
         </div>
       </div>
